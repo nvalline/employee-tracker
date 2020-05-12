eae4f2f789a790b01859da38594300a6e4cc33c7
vall4251n <58278138+nvalline@users.noreply.github.com>
2020-05-12 14:01:01 -0600

    added switch statement

--- index.js
+++ index.js
@@ -48,10 +48,51 @@ function runStart() {
             updateEmpMgr,
             deleteDept,
             deleteEmp,
-            deleteRole
+            deleteRole,
+            "Exit"
         ]
     })
         .then(answer => {
-            console.log(answer.action)
+            switch (answer.action) {
+                case viewDepts:
+                    console.log('viewDepts Hit')
+                    break;
+                case viewEmps:
+                    console.log('viewEmps Hit')
+                    break;
+                case viewRoles:
+                    console.log('viewRoles Hit')
+                    break;
+                case viewEmpByMgr:
+                    console.log('viewEmpByMgr Hit')
+                    break;
+                case addDept:
+                    console.log('addDept Hit')
+                    break;
+                case addEmp:
+                    console.log('addEmp Hit')
+                    break;
+                case addRole:
+                    console.log('addRole Hit')
+                    break;
+                case updateEmpRole:
+                    console.log('updateEmpRole Hit')
+                    break;
+                case updateEmpMgr:
+                    console.log('updateEmpMgr Hit')
+                    break;
+                case deleteDept:
+                    console.log('deleteDept Hit')
+                    break;
+                case deleteEmp:
+                    console.log('deleteEmp Hit')
+                    break;
+                case deleteRole:
+                    console.log('deleteRole Hit')
+                    break;
+                case "Exit":
+                    connection.end();
+                    break;
+            }
         })
 }
